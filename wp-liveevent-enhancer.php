5c51f5fd82f626ed8e920be7c8700095d13b8045
Harry Ramirez <harryrampr@gmail.com>
2024-01-27 09:41:36 -0400

    Initial Commit

---
+++ wp-liveevent-enhancer.php
@@ -0,0 +1,23 @@
+<?php declare( strict_types=1 );
+
+namespace HRPDEV\WpLiveEventEnhancer;
+
+/**
+ * Plugin Name: WP LiveEvent Enhancer
+ * Plugin URI:  https://yourwebsite.com
+ * Description: Enhances live-streaming events with streaming schedules, live-buttons, and more.
+ * Version:     1.0
+ * Author:      Harry Ramirez
+ * Author URI:  https://www.linkedin.com/in/harry-ramirez-picon/
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'WP_LIVEEVENT_ENHANCER_FILE' ) ) {
+	define( 'WP_LIVEEVENT_ENHANCER_FILE', __FILE__ );
+}
+
+require_once plugin_dir_path( WP_LIVEEVENT_ENHANCER_FILE ) . 'vendor/autoload.php';
+
+( new WPLiveEventEnhancer() )->run();
